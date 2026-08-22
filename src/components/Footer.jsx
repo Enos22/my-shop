a1c671a05a340ec2369f728eba32c8a13291feab
Emos Arenga <enos.arenga@gmail.com>
2026-08-22 14:49:53 +0300

    The Grocery shop footer details

---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+import styles from './Footer.module.css';
+export default function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <p>&copy; 2026 A Wholesome Grocery Shop. All rights reserved.</p>
+        </footer>
+    );
+}
