5ea82df3f71f3ab54c556500163a01108424e250
Emos Arenga <enos.arenga@gmail.com>
2026-08-24 12:47:20 +0300

    The Contact page of our Website

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,17 @@
+export default function Contact() {
+    return (
+        <main>
+            <h2>Contact Us</h2>
+            <p>If you have any questions or inquiries, please reach out to us at:</p>
+
+            <p>
+                <strong>Email:</strong> info@myshop.com
+            </p>
+            <p><strong>Phone:</strong> (254) 720-406-241
+            </p>
+            <p><strong>Location:</strong> Kilimani, Nairobi, Kenya
+            </p>
+
+        </main>
+    );
+}
